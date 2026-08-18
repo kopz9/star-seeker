b039bae0007071db1d355085096b0689603660b8
kopz9 <gustavoeduardo438@gmail.com>
2026-08-18 04:59:30 -0300

    fix: remove unused dependencies

--- backend/starseeker/src/main/java/me/kopz/starseeker/controller/ContractController.java
+++ backend/starseeker/src/main/java/me/kopz/starseeker/controller/ContractController.java
@@ -7,8 +7,6 @@ import me.kopz.starseeker.exception.AppException;
 import me.kopz.starseeker.service.ContractService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
